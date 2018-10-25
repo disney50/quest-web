b399509ebc45167881f304926100176550ec7917
disney50 <36032319+disney50@users.noreply.github.com>
2018-10-25 11:25:24 +0200

    register user

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-register',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
   maleStatus: boolean = true;
   femaleStatus: boolean = false;
+  newUser: User = {} as User;
 
   constructor() { }
 
@@ -22,6 +24,17 @@ export class RegisterComponent implements OnInit {
   femaleClickEvent() {
     this.maleStatus = false;
     this.femaleStatus = true;
+  }
+
+  registerNewUser(newUser: User) {
+    console.log(newUser);
+    if(this.maleStatus == true) {
+      newUser.gender = "MALE";
+    }
+    else {
+      newUser.gender = "FEMALE";
+    }
+    console.log(newUser);
   }
 
 }
