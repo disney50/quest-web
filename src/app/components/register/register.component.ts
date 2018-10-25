de8712b94e8d1db6f8f1df5d4275e9d45bc7ff37
disney50 <36032319+disney50@users.noreply.github.com>
2018-10-25 10:57:05 +0200

    male female selector

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  maleStatus: boolean = true;
+  femaleStatus: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  maleClickEvent() {
+    this.maleStatus = true;
+    this.femaleStatus = false;  
+  }
+
+  femaleClickEvent() {
+    this.maleStatus = false;
+    this.femaleStatus = true;
   }
 
 }
