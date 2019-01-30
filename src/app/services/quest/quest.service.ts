a32aeeb93c51618344ee310b1010749153c4ea38
disney50 <36032319+disney50@users.noreply.github.com>
2019-01-30 10:10:48 +0200

    quest service lint

--- src/app/services/quest/quest.service.ts
+++ src/app/services/quest/quest.service.ts
@@ -10,77 +10,77 @@ export class QuestService {
   explorerQuestsIds: string[] = [];
   possibleQuestsIds: string[] = [];
   possibleQuest: Quest = {} as Quest;
-  possibleQuests: Quest[] = []
+  possibleQuests: Quest[] = [];
   prerequisiteQuest: Quest = {} as Quest;
 
   constructor(private angularFirestore: AngularFirestore) { }
 
   submitQuest(planetName: string, userId: string, currentQuest: Quest) {
-    currentQuest.status = "moderating";    
+    currentQuest.status = 'moderating';
 
-    this.angularFirestore.collection(planetName + "/explorers/entries/" + userId + "/quests/").doc(currentQuest.questId).update(currentQuest.toData());
+    this.angularFirestore.collection(planetName + '/explorers/entries/' + userId + '/quests/')
+      .doc(currentQuest.questId).update(currentQuest.toData());
   }
 
   launchQuest(planetName: string, userId: string, currentQuest: Quest) {
-    currentQuest.status = "in_progress";
+    currentQuest.status = 'in_progress';
 
-    this.angularFirestore.collection(planetName + "/explorers/entries/" + userId + "/quests/").doc(currentQuest.questId).set(currentQuest.toData());
+    this.angularFirestore.collection(planetName + '/explorers/entries/' + userId + '/quests/')
+      .doc(currentQuest.questId).set(currentQuest.toData());
   }
 
-  checkIfPrerequisiteQuestCompleted(planetName: string, userId: string, possibleQuest: Quest): boolean {  
-    this.prerequisiteQuest = {} as Quest; 
-    
-    if(possibleQuest.prerequisites.length != 0) {
-      this.angularFirestore.collection(planetName + "/explorers/entries/" + userId + "/quests/")
-      .doc(possibleQuest.prerequisites[0]).ref.onSnapshot(snapShot => {
+  checkIfPrerequisiteQuestCompleted(planetName: string, userId: string, possibleQuest: Quest): boolean {
+    this.prerequisiteQuest = {} as Quest;
 
-        if(snapShot.exists) {          
-          this.prerequisiteQuest = new Quest(snapShot.id, snapShot.data() as QuestData);          
+    if (possibleQuest.prerequisites.length !== 0) {
+      this.angularFirestore.collection(planetName + '/explorers/entries/' + userId + '/quests/')
+        .doc(possibleQuest.prerequisites[0]).ref.onSnapshot(snapShot => {
 
-          if(this.prerequisiteQuest.status == "completed") {
-            possibleQuest.isAvailable = true;            
+          if (snapShot.exists) {
+            this.prerequisiteQuest = new Quest(snapShot.id, snapShot.data() as QuestData);
+
+            if (this.prerequisiteQuest.status === 'completed') {
+              possibleQuest.isAvailable = true;
+            } else {
+              possibleQuest.isAvailable = false;
+            }
           }
-          else {
-            possibleQuest.isAvailable = false;
-          }   
-        }
 
-      });
-    }
-    else {
+        });
+    } else {
       possibleQuest.isAvailable = true;
     }
 
     return possibleQuest.isAvailable;
   }
 
-  getPossibleQuests(planetQuests: Quest[], explorerQuests: Quest[], planetName: string, userId: string): Quest[] {  
+  getPossibleQuests(planetQuests: Quest[], explorerQuests: Quest[], planetName: string, userId: string): Quest[] {
     this.possibleQuests = [];
-    
+
     planetQuests.forEach(planetQuest => {
-      if(this.planetQuestsIds.indexOf(planetQuest.questId) === -1) {
+      if (this.planetQuestsIds.indexOf(planetQuest.questId) === -1) {
         this.planetQuestsIds.push(planetQuest.questId);
       }
-    });  
-            
+    });
+
     explorerQuests.forEach(explorerQuest => {
-      if(this.explorerQuestsIds.indexOf(explorerQuest.questId) === -1) {
+      if (this.explorerQuestsIds.indexOf(explorerQuest.questId) === -1) {
         this.explorerQuestsIds.push(explorerQuest.questId);
       }
     });
 
-    this.planetQuestsIds.forEach(planetQuestId => {      
-      if(this.explorerQuestsIds.indexOf(planetQuestId) === -1) {        
-        this.possibleQuest = planetQuests.find(function(planetQuest) { 
-          return planetQuest.questId === planetQuestId; 
-        }); 
+    this.planetQuestsIds.forEach(planetQuestId => {
+      if (this.explorerQuestsIds.indexOf(planetQuestId) === -1) {
+        this.possibleQuest = planetQuests.find(function (planetQuest) {
+          return planetQuest.questId === planetQuestId;
+        });
 
         this.possibleQuests.push(this.possibleQuest);
       }
     });
 
     this.possibleQuests.forEach(possibleQuest => {
-      possibleQuest.isAvailable = this.checkIfPrerequisiteQuestCompleted(planetName, userId, possibleQuest)
+      possibleQuest.isAvailable = this.checkIfPrerequisiteQuestCompleted(planetName, userId, possibleQuest);
     });
 
     return this.possibleQuests;
