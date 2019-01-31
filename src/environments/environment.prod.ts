bd2bc33419c1d39b6d450c11984b2ba1ee50a5f9
disney50 <36032319+disney50@users.noreply.github.com>
2019-01-31 14:24:40 +0200

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDTnl6-D3P_ACn2PE1hQnLKmK9WV74MzCs',
+    authDomain: 'quest-98e6d.firebaseapp.com',
+    databaseURL: 'https://quest-98e6d.firebaseio.com',
+    projectId: 'quest-98e6d',
+    storageBucket: 'quest-98e6d.appspot.com',
+    messagingSenderId: '94325588580'
+  }
 };
