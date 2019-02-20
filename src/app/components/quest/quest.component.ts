f1d1091a1a84ea6c838698b54ce75298468bc60a
disney50 <36032319+disney50@users.noreply.github.com>
2019-02-20 15:14:19 +0200

    xp finetune

--- src/app/components/quest/quest.component.ts
+++ src/app/components/quest/quest.component.ts
@@ -42,7 +42,7 @@ export class QuestComponent implements OnInit {
   uploads = [];
   image = null;
 
-  newXP = null;
+  newXP = 0;
   failedStatus = false;
   relaunchStatus = false;
   completedStatus = true;
@@ -97,202 +97,202 @@ export class QuestComponent implements OnInit {
   }
 
   moderateQuest() {
-    if (this.newXP === null) {
-      this.message = 'You forgot to assign XP...';
-    } else {
-      if (this.failedStatus) {
-        this.selectedQuest.status = 'inprogress';
-      } else if (this.relaunchStatus) {
-        this.selectedQuest.status = 'inprogress';
-      } else if (this.completedStatus) {
+    if (this.failedStatus) {
+      this.selectedQuest.status = 'inprogress';
+    } else if (this.relaunchStatus) {
+      this.selectedQuest.status = 'inprogress';
+    } else if (this.completedStatus) {
+      if (this.newXP === 0) {
+        this.message = 'You forgot to assign XP...';
+      } else {
         this.selectedQuest.status = 'completed';
       }
-
-      this.selectedExplorer.xp = this.selectedExplorer.xp + this.newXP;
-
-      this.questService.moderateQuest(this.currentPlanet.name, this.selectedExplorer, this.selectedQuest);
-
-      this.navigateExplorer();
     }
+
+    this.selectedExplorer.xp = this.selectedExplorer.xp + this.newXP;
+
+    this.questService.moderateQuest(this.currentPlanet.name, this.selectedExplorer, this.selectedQuest);
+
+    this.navigateExplorer();
   }
 
-  fileClicked(selectedUploadName: string) {
-    const storageRef = firebase.storage().ref()
-      .child(this.currentPlanet.name + '/' + this.selectedExplorer.userId + '/' + this.selectedQuest.questId + '/' + selectedUploadName);
-    storageRef.getDownloadURL().then(url => {
-      this.image = url;
-    });
-  }
+fileClicked(selectedUploadName: string) {
+  const storageRef = firebase.storage().ref()
+    .child(this.currentPlanet.name + '/' + this.selectedExplorer.userId + '/' + this.selectedQuest.questId + '/' + selectedUploadName);
+  storageRef.getDownloadURL().then(url => {
+    this.image = url;
+  });
+}
 
-  getDocuments() {
-    this.fileService.getCollectionForUpload(this.currentPlanet.name, this.selectedExplorer.userId, this.selectedQuest.questId)
-      .valueChanges().subscribe(documents => {
-        documents.forEach(document => {
-          const upload = {} as Upload;
-          upload.name = document.name;
-          upload.timestamp = document.timestamp;
-          this.uploads.push(upload);
-        });
+getDocuments() {
+  this.fileService.getCollectionForUpload(this.currentPlanet.name, this.selectedExplorer.userId, this.selectedQuest.questId)
+    .valueChanges().subscribe(documents => {
+      documents.forEach(document => {
+        const upload = {} as Upload;
+        upload.name = document.name;
+        upload.timestamp = document.timestamp;
+        this.uploads.push(upload);
       });
+    });
 
+}
+
+// getUploads() {
+//   this.documents.forEach(document => {
+//     this.fileService.downloadFileFromStorage(this.currentPlanet.name, this.selectedExplorer.userId,
+//       this.selectedQuest.questId, document.name)
+//       .then(url => {
+//         const upload = {} as Upload;
+//         const xmlHttpRequest = new XMLHttpRequest();
+//         xmlHttpRequest.responseType = 'blob';
+//         xmlHttpRequest.onload = () => {
+//           upload.name = document.name;
+//           const blob = xmlHttpRequest.response;
+//           upload.link = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+//           this.uploads.push(upload);
+//         };
+//         xmlHttpRequest.open('GET', url);
+//         xmlHttpRequest.send();
+//       });
+//   });
+// }
+
+checkStatus() {
+  if (this.selectedQuest.status === 'inprogress'
+    || this.selectedQuest.status === 'moderating'
+    || this.selectedQuest.status === 'completed') {
+    this.store.dispatch(new actions.RequestGetComments(this.currentPlanet.name, this.signedInUser.userId, this.selectedQuest.questId));
+    this.sliceAllComments();
+    this.setStatus();
+  } else {
+    this.isUndefined = true;
   }
+}
 
-  // getUploads() {
-  //   this.documents.forEach(document => {
-  //     this.fileService.downloadFileFromStorage(this.currentPlanet.name, this.selectedExplorer.userId,
-  //       this.selectedQuest.questId, document.name)
-  //       .then(url => {
-  //         const upload = {} as Upload;
-  //         const xmlHttpRequest = new XMLHttpRequest();
-  //         xmlHttpRequest.responseType = 'blob';
-  //         xmlHttpRequest.onload = () => {
-  //           upload.name = document.name;
-  //           const blob = xmlHttpRequest.response;
-  //           upload.link = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
-  //           this.uploads.push(upload);
-  //         };
-  //         xmlHttpRequest.open('GET', url);
-  //         xmlHttpRequest.send();
-  //       });
-  //   });
-  // }
+setStatus() {
+  if (this.selectedQuest.status === 'inprogress') {
+    this.isInProgress = true;
+  } else if (this.selectedQuest.status === 'moderating') {
+    this.isModerating = true;
+  } else if (this.selectedQuest.status === 'completed') {
+    this.isCompleted = true;
+  }
+}
 
-  checkStatus() {
-    if (this.selectedQuest.status === 'inprogress'
-      || this.selectedQuest.status === 'moderating'
-      || this.selectedQuest.status === 'completed') {
-      this.store.dispatch(new actions.RequestGetComments(this.currentPlanet.name, this.signedInUser.userId, this.selectedQuest.questId));
-      this.sliceAllComments();
-      this.setStatus();
-    } else {
-      this.isUndefined = true;
+sendClicked(newComment: string) {
+  if (!newComment) {
+    this.message = 'You forgot to write a comment';
+  } else {
+    if (this.userSignedIn) {
+      this.commentService.createComment(this.currentPlanet.name, this.signedInUser.userId, this.selectedQuest, newComment, false);
+    } else if (this.moderatorSignedIn) {
+      this.commentService.createComment(this.currentPlanet.name, this.selectedExplorer.userId, this.selectedQuest, newComment, true);
     }
   }
+}
 
-  setStatus() {
-    if (this.selectedQuest.status === 'inprogress') {
-      this.isInProgress = true;
-    } else if (this.selectedQuest.status === 'moderating') {
-      this.isModerating = true;
-    } else if (this.selectedQuest.status === 'completed') {
-      this.isCompleted = true;
-    }
-  }
+fileSelected(event) {
+  this.selectedFile = event.target.files[0];
+}
 
-  sendClicked(newComment: string) {
-    if (!newComment) {
-      this.message = 'You forgot to write a comment';
-    } else {
-      if (this.userSignedIn) {
-        this.commentService.createComment(this.currentPlanet.name, this.signedInUser.userId, this.selectedQuest, newComment, false);
-      } else if (this.moderatorSignedIn) {
-        this.commentService.createComment(this.currentPlanet.name, this.selectedExplorer.userId, this.selectedQuest, newComment, true);
-      }
-    }
-  }
-
-  fileSelected(event) {
-    this.selectedFile = event.target.files[0];
-  }
-
-  submitClicked() {
-    if (!this.selectedFile) {
-      this.message = 'You forgot to upload a file';
-    } else {
-      this.fileService
-        .uploadFileToStorage(this.selectedFile, this.currentPlanet.name, this.signedInUser.userId, this.selectedQuest.questId);
-      this.questService.submitQuest(this.currentPlanet.name, this.signedInUser.userId, this.selectedQuest);
-      this.navigateDashboard();
-    }
-  }
-
-  launchClicked() {
-    this.questService.launchQuest(this.currentPlanet.name, this.signedInUser.userId, this.selectedQuest);
+submitClicked() {
+  if (!this.selectedFile) {
+    this.message = 'You forgot to upload a file';
+  } else {
+    this.fileService
+      .uploadFileToStorage(this.selectedFile, this.currentPlanet.name, this.signedInUser.userId, this.selectedQuest.questId);
+    this.questService.submitQuest(this.currentPlanet.name, this.signedInUser.userId, this.selectedQuest);
     this.navigateDashboard();
   }
+}
 
-  sliceHasLoginSucceeded() {
-    combineLatest(
-      this.store.select(selectors.moderatorSignedIn),
-      this.store.select(selectors.userSignedIn)
-    ).subscribe(combinedValue => {
-      this.moderatorSignedIn = combinedValue[0];
-      this.userSignedIn = combinedValue[1];
-      if (!this.moderatorSignedIn && !this.userSignedIn) {
-        this.navigateLogin();
+launchClicked() {
+  this.questService.launchQuest(this.currentPlanet.name, this.signedInUser.userId, this.selectedQuest);
+  this.navigateDashboard();
+}
+
+sliceHasLoginSucceeded() {
+  combineLatest(
+    this.store.select(selectors.moderatorSignedIn),
+    this.store.select(selectors.userSignedIn)
+  ).subscribe(combinedValue => {
+    this.moderatorSignedIn = combinedValue[0];
+    this.userSignedIn = combinedValue[1];
+    if (!this.moderatorSignedIn && !this.userSignedIn) {
+      this.navigateLogin();
+    }
+  });
+}
+
+sliceSignedInUser() {
+  this.store.select(selectors.signedInUser).subscribe(signedInUser => {
+    if (this.moderatorSignedIn || this.userSignedIn) {
+      this.signedInUser = signedInUser;
+    }
+  });
+}
+
+sliceCurrentPlanet() {
+  this.store.select(selectors.currentPlanet).subscribe(currentPlanet => {
+    if (this.moderatorSignedIn || this.userSignedIn) {
+      this.currentPlanet = currentPlanet;
+    }
+  });
+}
+
+sliceSelectedExplorer() {
+  this.store.select(selectors.selectedExplorer).subscribe(selectedExplorer => {
+    if (this.moderatorSignedIn) {
+      this.selectedExplorer = selectedExplorer;
+    }
+  });
+}
+
+sliceSelectedQuest() {
+  this.store.select(selectors.selectedQuest).subscribe(selectedQuest => {
+    if (this.userSignedIn) {
+      this.selectedQuest = selectedQuest;
+      this.checkStatus();
+      this.sliceAllComments();
+    } else if (this.moderatorSignedIn) {
+      this.selectedQuest = selectedQuest;
+      this.store.dispatch(new actions.RequestGetComments(this.currentPlanet.name, this.selectedExplorer.userId,
+        this.selectedQuest.questId));
+      this.setStatus();
+      this.sliceAllComments();
+      this.questService.updateLastViewCommentDate(this.currentPlanet.name, this.selectedExplorer.userId, this.selectedQuest);
+    }
+  });
+}
+
+sliceAllComments() {
+  this.store.select(selectors.allComments).subscribe(allComments => {
+    if (this.moderatorSignedIn || this.userSignedIn) {
+      this.allComments = allComments;
+    }
+  });
+}
+
+sliceCurrentQuestExists() {
+  this.store.select(selectors.currentQuestExists).subscribe(currentQuestExists => {
+    if (this.moderatorSignedIn || this.userSignedIn) {
+      if (currentQuestExists) {
+        this.currentQuestExists = true;
+      } else if (!currentQuestExists) {
+        this.currentQuestExists = false;
       }
-    });
-  }
+    }
+  });
+}
 
-  sliceSignedInUser() {
-    this.store.select(selectors.signedInUser).subscribe(signedInUser => {
-      if (this.moderatorSignedIn || this.userSignedIn) {
-        this.signedInUser = signedInUser;
-      }
-    });
-  }
-
-  sliceCurrentPlanet() {
-    this.store.select(selectors.currentPlanet).subscribe(currentPlanet => {
-      if (this.moderatorSignedIn || this.userSignedIn) {
-        this.currentPlanet = currentPlanet;
-      }
-    });
-  }
-
-  sliceSelectedExplorer() {
-    this.store.select(selectors.selectedExplorer).subscribe(selectedExplorer => {
-      if (this.moderatorSignedIn) {
-        this.selectedExplorer = selectedExplorer;
-      }
-    });
-  }
-
-  sliceSelectedQuest() {
-    this.store.select(selectors.selectedQuest).subscribe(selectedQuest => {
-      if (this.userSignedIn) {
-        this.selectedQuest = selectedQuest;
-        this.checkStatus();
-        this.sliceAllComments();
-      } else if (this.moderatorSignedIn) {
-        this.selectedQuest = selectedQuest;
-        this.store.dispatch(new actions.RequestGetComments(this.currentPlanet.name, this.selectedExplorer.userId,
-          this.selectedQuest.questId));
-        this.setStatus();
-        this.sliceAllComments();
-        this.questService.updateLastViewCommentDate(this.currentPlanet.name, this.selectedExplorer.userId, this.selectedQuest);
-      }
-    });
-  }
-
-  sliceAllComments() {
-    this.store.select(selectors.allComments).subscribe(allComments => {
-      if (this.moderatorSignedIn || this.userSignedIn) {
-        this.allComments = allComments;
-      }
-    });
-  }
-
-  sliceCurrentQuestExists() {
-    this.store.select(selectors.currentQuestExists).subscribe(currentQuestExists => {
-      if (this.moderatorSignedIn || this.userSignedIn) {
-        if (currentQuestExists) {
-          this.currentQuestExists = true;
-        } else if (!currentQuestExists) {
-          this.currentQuestExists = false;
-        }
-      }
-    });
-  }
-
-  ngOnInit() {
-    this.sliceHasLoginSucceeded();
-    this.sliceSignedInUser();
-    this.sliceCurrentPlanet();
-    this.sliceSelectedExplorer();
-    this.sliceSelectedQuest();
-    this.sliceCurrentQuestExists();
-    this.getDocuments();
-  }
+ngOnInit() {
+  this.sliceHasLoginSucceeded();
+  this.sliceSignedInUser();
+  this.sliceCurrentPlanet();
+  this.sliceSelectedExplorer();
+  this.sliceSelectedQuest();
+  this.sliceCurrentQuestExists();
+  this.getDocuments();
+}
 
 }
